ebee319f87e2cbc698d64b30a2b19e2a2ba0daf8
Atsudani <atsudani@gmail.com>
2023-08-31 18:08:45 -0400

    Accion agregado en prueba.ts

--- tipos/prueba.js
+++ tipos/prueba.js
@@ -1,3 +1,4 @@
 "use strict";
 const variable = "Esto es una cadena de texto";
 console.log(variable);
+const accion = 'Practicando push a repositorio remoto';
--- tipos/prueba.ts
+++ tipos/prueba.ts
@@ -1,3 +1,5 @@
 const variable: string = "Esto es una cadena de texto";
 
 console.log(variable);
+
+const accion: string = 'Practicando push a repositorio remoto';
