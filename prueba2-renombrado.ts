ef9c042cc8cd74fea822bd427028520610c447ef
Atsushi <atsudani@gmail.com>
2023-09-01 13:53:23 -0400

    prueba2 modificado con jojojojo

--- prueba2-renombrado.ts
+++ prueba2-renombrado.ts
@@ -1,5 +1,5 @@
 (()=>{
-    const nombre: string = 'jejejeje';
+    const nombre: string = 'jojojojojo';
     const nombre1: string = 'jejejeje1';
     const nombre2: string = 'jejejeje1';
     const nombre3: string = 'jejejeje1';
