5a3c88ba39d9148582444bac8e650cbb598dca83
Atsushi <atsudani@gmail.com>
2023-09-02 10:06:00 -0400

    tuplas agreagado

---
+++ tipos/tupes.js
@@ -0,0 +1,11 @@
+"use strict";
+(() => {
+    //definicion de arreglo
+    const hero1 = ['Dr Strangre', 100];
+    const hero2 = [1, 2];
+    //definicion de tupla
+    //la tupla es como un arreglo estricto con los tipos de datos
+    const hero3 = ['abc', 50];
+    hero3[0] = 'adfasdfasdf';
+    hero3[1] = 5;
+})();
---
+++ tipos/tupes.ts
@@ -0,0 +1,11 @@
+(()=>{
+    //definicion de arreglo
+    const hero1 = ['Dr Strangre',100];
+    const hero2: (string | number)[] = [1,2];
+
+    //definicion de tupla
+    //la tupla es como un arreglo estricto con los tipos de datos
+    const hero3: [string,number] = ['abc',50];
+    hero3[0] = 'adfasdfasdf';
+    hero3[1] = 5;
+})()
